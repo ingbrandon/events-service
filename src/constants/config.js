1b0f3fb4ca214ef58fb3160e0f6e51c46dd56b48
Brandon Henao <ingeniero.desarrollo029@serviciosmarttmt.com>
2020-08-31 13:52:36 -0500

    Se eliminan archivos no necesarios

--- src/constants/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var connBD = {
-    url: 'mongodb://localhost:27017/pusher'
-};
-exports.default = connBD;
-//# sourceMappingURL=config.js.map
--- src/constants/cors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var corsOptions = {
-    origin: '*',
-    methods: '*',
-    allowedHeaders: '*'
-};
-exports.default = corsOptions;
-//# sourceMappingURL=cors.js.map
